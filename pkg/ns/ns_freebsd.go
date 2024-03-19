b33cf91fe2a67e188b43a0196ba83d24f9d2004d
SoumaSakaguchi <mokemoke1319@gmail.com>
2024-03-19 16:08:59 +0900

    pkg/ns/ns_freebsd.go: Add symlink pkg

---
+++ pkg/ns/ns_freebsd.go
@@ -0,0 +1,49 @@
+package ns
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gizahNL/gojail"
+)
+
+const (
+	defaultJailDir = "/var/lib/runj/jails/"
+	defaultLinkName = "netns"
+	defaultNsDir = "/var/run/netns/"
+)
+
+func GetCurrentNsJail(childJail gojail.Jail) (gojail.Jail, error) {
+	nsName, err := getNsPath(childJail.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	nsjail, err := gojail.JailGetByName(nsName)
+	if err != nil {
+		return nil, err
+	}
+
+	return nsJail, nil
+}
+
+func getNsPath(jailID string) (string, error) {
+	path := filepath.Join(defaultJailDir, jailID, defaultLinkName)
+
+	info, err := os.Lstat(path)
+	if err != nil {
+		return "", err
+	}
+
+	var realPath string
+	if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+		realPath, err = os.Readlink(path)
+		if err != nil {
+			return "", err
+		}
+	} else{
+		return "", fmt.Errorf("Path is not symboliclink.")
+	}
+	return filepath.Base(realPath), nil
+}
